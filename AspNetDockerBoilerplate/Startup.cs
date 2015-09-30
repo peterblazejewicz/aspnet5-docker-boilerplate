1bae096d861fd19ccb3c2a7d66c277295b2dfc6f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-30 22:24:29 +0200

    Fix unused imports noise

--- AspNetDockerBoilerplate/Startup.cs
+++ AspNetDockerBoilerplate/Startup.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
 
 namespace AspNetDockerBoilerplate
 {
